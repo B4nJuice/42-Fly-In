d0f292ef4469093afbf75f57f5442996e2b972d9
Girard Louis <lgirard@student.42lyon.fr>
2026-05-19 09:05:21 +0200

    Fix: max_drones is only positive even if blocked

--- src/network/metadata/zone_metadata.py
+++ src/network/metadata/zone_metadata.py
@@ -112,9 +112,10 @@ class ZoneMetadata(MetadataInterface):
         if diff := (self.metadata.keys() - self.default_metadata.keys()):
             raise MetadataError(f"Invalid metadata fields : {diff}")
 
-        if cast(int, self.metadata.get("max_drones")) < 0:
+        if cast(int, self.metadata.get("max_drones")) <= 0 and not\
+            cast(ZoneType, self.metadata.get("zone")) == ZoneType.BLOCKED:
             raise MetadataError(
-                    "max_drones field has to be a non-negative integer."
+                    "max_drones field has to be a positive integer."
                 )
 
     def set_start_hub(self) -> None:
